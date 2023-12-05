a171b94508d5bd5f34e30011c968d9a7fcbefe42
cherniaev egor <chrnyaevek@gmail.com>
2023-12-05 20:44:03 +0100

    Fetch fpic dataset

---
+++ core/scripts/fetch_datasets.py
@@ -0,0 +1,11 @@
+import os
+import dataset_tools as dtools
+
+
+from core.settings import DATASETS_FOLDER
+
+
+# FPIC-Component Dataset
+# Labeled PCB dataset of 6260 images
+dtools.download(dataset='FPIC-Component',
+                dst_dir=f'{DATASETS_FOLDER}/fpic-component-dataset')
---
+++ core/settings.py
@@ -0,0 +1,3 @@
+WORKSPACE_FOLDER = '/media/egor/T7/vut_dp_project_workspace'
+ASSETS_FOLDER = f'{WORKSPACE_FOLDER}/assets'
+DATASETS_FOLDER = f'{ASSETS_FOLDER}/datasets'
