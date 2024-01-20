7906966a9707d88acbb74f301cc87509e788a4d8
cherniaev egor <chrnyaevek@gmail.com>
2024-01-20 23:15:50 +0100

    Update web wireframe

--- ros_catkin_ws/src/automatic_optical_inspection/scripts/image_acquisition_server.py
+++ ros_catkin_ws/src/automatic_optical_inspection/scripts/image_acquisition_server.py
@@ -7,19 +7,20 @@
 """
 
 import rospy
-from automatic_optical_inspection.srv import ImageAcquisition
+from automatic_optical_inspection.srv import ImageAcquisition, ImageAcquisitionRequest, ImageAcquisitionResponse
 
-node_name = "image acquisition"
+node_name = "image_acquisition"
+fs = rospy.get_param("/file_system")
 
-
-def handle_image_acquisition(req):
+def handle_image_acquisition(req: ImageAcquisitionRequest) -> ImageAcquisitionResponse:
     rospy.loginfo("Real image requested")
     raise NotImplementedError()
 
 
 if __name__ == "__main__":
-    rospy.loginfo(f"{node_name.capitalize()} is alive!")
-    rospy.init_node("image_acquisition")
-    s = rospy.Service("acquire_image", ImageAcquisition, handle_image_acquisition)
+    rospy.init_node(node_name)
+    s = rospy.Service("image_acquisition/acquire", ImageAcquisition, handle_image_acquisition)
+
     rospy.loginfo(f"Spinning {node_name}...")
+    rospy.loginfo(f'File system: {fs}')
     rospy.spin()
